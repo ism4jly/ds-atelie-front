bd558bf80ff0c0f25be2a35f97f95fc3afc8da9b
ism4jly <ismaelpaulofreitas@gmail.com>
2024-03-21 15:03:56 -0300

    item page static

--- src/pages/aneis/index.tsx
+++ src/pages/aneis/index.tsx
@@ -1,12 +1,34 @@
+import { Container } from "../../components/container";
+import anel from "../../assets/solitario.png"
+import { Link } from "react-router-dom";
 
-
-
+import { FaWhatsapp } from "react-icons/fa";
 
 
 export function Aneis(){
     return(
-        <div>
-            Aneis
-        </div>
+        <Container>
+            <div className="max-w-[270px]">
+                <Link to=''>
+                    <img
+                        className="h-[270px]"
+                        src={anel}
+                    />
+                    <p>
+                        Anel Solitário Em Ouro 18k 
+                        Delicado com Zircônia Cristal Cravejada
+                    </p>
+                </Link>
+
+                <p className="mt-2 text-2xl font-semibold">R$ 1.625,00</p>
+
+                <hr className="mt-5 mb-5 border-1 border-gray-300" />
+
+                <Link to='' className="bg-[#D3ADA3] flex p-3 items-center justify-center rounded-lg">
+                    <span className="font-bold mr-2 uppercase text-white">Comprar agora</span>
+                    <FaWhatsapp color={'white'} size={23}/>
+                </Link>
+            </div>
+        </Container>
     )
 }
--- src/pages/brincos/index.tsx
+++ src/pages/brincos/index.tsx
@@ -1,11 +1,35 @@
+import { Container } from "../../components/container";
+import brinco from "../../assets/argola-trabalhada.png"
+import { Link } from "react-router-dom";
+
+import { FaWhatsapp } from "react-icons/fa";
 
 
 
 
 export function Brincos(){
     return(
-        <div>
-            Brincos
-        </div>
+        <Container>
+            <div className="max-w-[270px]">
+                <Link to=''>
+                    <img
+                        className="h-[270px]"
+                        src={brinco}
+                    />
+                    <p>
+                        Brinco De Prata Leve Argola Trabalhada
+                    </p>
+                </Link>
+
+                <p className="mt-2 text-2xl font-semibold">R$ 135,00</p>
+
+                <hr className="mt-5 mb-5 border-1 border-gray-300" />
+
+                <Link to='' className="bg-[#D3ADA3] flex p-3 items-center justify-center rounded-lg">
+                    <span className="font-bold mr-2 uppercase text-white">Comprar agora</span>
+                    <FaWhatsapp color={'white'} size={23}/>
+                </Link>
+            </div>
+        </Container>
     )
 }
